1a485ad372617b4d19243b27c41b93c081d9a481
yasenstar <xiaoqizhao@outlook.com>
2019-05-04 17:16:25 +0800

    update

--- circle.py
+++ circle.py
@@ -1,10 +1,9 @@
-import math
-
-print("你好，请输入要计算的最大半径，我们将从半径为0开始输出")
-r = int(input())
-i=0
-print("半径   周长    面积")
-
-while i <= r:
-    print(i, 2*math.pi*i, math.pi*math.pow(i,2))
-    i=i+0.1
+import turtle
+turtle.color('orange')
+turtle.pensize(3)
+turtle.circle(75)
+turtle.penup()
+turtle.goto(0, -200)
+turtle.pendown()
+turtle.circle(100)
+turtle.done()
---
+++ star_line.py
@@ -0,0 +1,17 @@
+#!/usr/bin/phthon
+# -*- coding: UTF-8 -*-
+
+import tkinter
+# import tkMessageBox
+
+# -*- coding: cp936 -*-
+from tkinter import *
+root = Tk()
+
+cv = Canvas(root, bg = 'white')
+
+cv.create_reccircle(0,0,10)
+cv.pack()
+root.mainloop()
+
+top.mainloop()
