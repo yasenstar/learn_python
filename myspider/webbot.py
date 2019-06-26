4f5f9d197e81c6cc33c63db02f93bccae88649d5
yasenstar <xiaoqizhao@outlook.com>
2019-06-26 23:52:17 +0800

    spider update

--- myspider/webbot.py
+++ myspider/webbot.py
@@ -7,7 +7,8 @@ import string
 start = input("Where would you like to start searching?\n")
 br = mechanize.Browser()
 r = br.open(start)
-html = r.read()
+# html = r.read()
+html = "http://www.pbc.gov.cn/zhengwugongkai/127924/128041/2951606/1923625/1923629/d6d180ae/index4.html"
 
 soup = BeautifulSoup(html)
 for link in soup.find_all('a'):
