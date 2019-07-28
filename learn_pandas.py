d0cd87b8abfea0285d20fcb2f90dae4996dcb683
yasenstar <xiaoqizhao@outlook.com>
2019-07-28 14:57:11 +0800

    Create learn_pandas.py

---
+++ learn_pandas.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame([10, 20, 30, 40], columns=['numbers'], index=['a', 'b', 'c', 'd'])
+
+print(df)
+
+df['floats'] = (1.5, 2.5, 3.5, 4.5)
+
+print(df)
+
+df['names'] = pd.DataFrame(['Yves', 'Guido', 'Felix', 'Francesc'], index = ['d', 'a', 'b', 'c'])
+
+print(df)
+
+df = df.append(pd.DataFrame({'numbers': 100, 'floats': 5.75, 'names': 'Henry'}, index=['z']))
+
+print(df)
