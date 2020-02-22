5b0ac8941e339cf6b7cc03e80fa95d7beefc6757
yasenstar <xiaoqizhao@outlook.com>
2020-02-22 16:25:52 -0500

    correct encoding issue

--- times_linux_cn.py
+++ times_linux_cn.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import random
-print("10以内（含10）的乘法练习").decode('UTF-8').encode(type)
+print("10以内（含10）的乘法练习")
 k=1
 count=0
 correct=0
@@ -13,7 +13,7 @@ while want_to_quit != 1:
         a = int(round(random.random()*10,0))
         b = int(round(random.random()*10,0))
 
-        print("请计算：", a, " x ", b, " = ?").decode('UTF-8').encode(type)
+        print("请计算：", a, " x ", b, " = ?")
         if int(input()) == a * b:
             print("正确\n")
             k=1
@@ -22,12 +22,12 @@ while want_to_quit != 1:
                 correct=correct+1
             first_time_correct = 1
             if count % 5 == 0:
-                print("继续?(y or n)").decode('UTF-8').encode(type)
+                print("继续?(y or n)")
                 if input() == "n":
                     want_to_quit = 1
                     exit
         else:
-            print("再想想").decode('UTF-8').encode(type)
+            print("再想想")
             k=0
             first_time_correct = 0
 
