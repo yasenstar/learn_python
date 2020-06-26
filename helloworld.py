900df67f88dba8fe48952a1eb438a7997b1049b6
Yasen <xiaoqizhao@outlook.com>
2020-06-26 18:47:26 -0400

    20200626

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,18 @@
-# -*- coding: utf-8 -*-
-msg = "Hello World"
-print (msg)
+# print ("hello world")
+# print ("nice world")
+# print ("化繁为简")
+# print ('hellohellohellohellohellohellohellohellohellohellohellohellohellohelloh\
+# 	ellohellohellohellohellohellohellohellohellohellohello')
+# print (123+456)
+# for i in range (1, 15):
+# 	print (i, " good")
+
+name = "亚森"
+
+print ("欢迎"+name+"光临")
+
+print ('欢迎', name, '光临')
+
+print ('欢迎%s光临'%name)
+
+print (f'欢迎{name}光临')
