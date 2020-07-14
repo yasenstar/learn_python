4c9820dd657e7f51a1936cee2962571755461fed
Yasen <xiaoqizhao@outlook.com>
2020-07-14 16:41:50 -0400

    update

---
+++ numeric/divide_exercise.py
@@ -0,0 +1,12 @@
+# 有余数的除法
+
+# English
+# number1 = int(input("please input first number: "))
+# number2 = int(input("please input second number: "))
+
+# Chinese
+number1 = int(input("请输入被除数: "))
+number2 = int(input("请输入除数: "))
+
+print ("=" * 50)
+print(number1, "/", number2, "=", number1//number2, "......", number1%number2)
